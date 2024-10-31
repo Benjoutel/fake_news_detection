3ed4cb25f34895ca89d598eaedfa54b8a4e112a5
aurelien cardon <aureliencardon85@gmail.com>
2024-10-31 10:43:03 +0100

    preprocess and model done

--- files/images.py
+++ files/images.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 import numpy as np
+from keras.models import load_model
 
 def load_and_preprocess_image(image_path):
 
@@ -10,3 +11,11 @@ def load_and_preprocess_image(image_path):
     image = np.expand_dims(image, axis=0)
 
     return image
+
+def predict(model_path, image):
+
+    model = load_model(model_path)
+
+    prediction = model.predict(image)
+
+    return prediction
