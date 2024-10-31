1e3864316b8d6153feda9ba51451a744c64fd738
aurelien cardon <aureliencardon85@gmail.com>
2024-10-31 09:49:47 +0100

    preprocess image for pred

--- files/images.py
+++ files/images.py
@@ -1,13 +1,12 @@
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import numpy as np
 
-def load_and_preprocess_image(image_path, target_size=(32, 32)):
+def load_and_preprocess_image(image_path):
 
-    image = load_img(image_path, target_size=target_size)
-    def preprocess_images(image, label):
-        image = preprocess_input(image)
-        return image, label
-
-    image = image.map(preprocess_images)
+    image = load_img(image_path, target_size=(32,32))
+    image = img_to_array(image)
+    image = preprocess_input(image)
+    image = np.expand_dims(image, axis=0)
 
     return image
