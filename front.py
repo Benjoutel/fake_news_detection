8c25ba71b064333b98faa4206f07f40087781a47
Benjamin Moutel <32609318+Benjoutel@users.noreply.github.com>
2024-11-19 15:33:08 +0100

    Update front.py

--- front.py
+++ front.py
@@ -6,7 +6,7 @@ import io
 from io import BytesIO
 import cv2
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"  # need to specify access url to pytesseract for streamlit cloud
+pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"  # need to specify access url to pytesseract for streamlit cloud
 import numpy as np
 from googlesearch import search
 import os
